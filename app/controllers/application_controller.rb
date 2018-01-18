f20727f7534929045506a3d180cc7213a0f11274
Unknown <marcin.krykowski@gmail.com>
2018-01-18 14:44:11 +0100

    Unsaved file added :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with name: "user", password: "user", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
 end
